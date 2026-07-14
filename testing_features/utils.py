3336a2b8fba43c3de827b9ee109f9c231bd389a7
Anshul Garg <anshulgarg1133@gmail.com>
2026-07-14 21:53:19 +0530

    Update utils.py

--- testing_features/utils.py
+++ testing_features/utils.py
@@ -1,6 +1,6 @@
-#cleaning of text
 import re
 
+#cleaning of text
 def clean_chunk(text):
     text = re.sub(r'\s+', ' ', text)           # collapse multiple spaces/newlines into one space
     text = re.sub(r'\bPage \d+\b', '', text)    # remove things like "Page 3"
@@ -10,3 +10,7 @@ def clean_chunk(text):
 def split_into_claims(answer_text):
     sentences = re.split(r'(?<=[.!?])\s+', answer_text)   # split on sentence-ending punctuation
     return [s.strip() for s in sentences if s.strip()]
+
+#remove citation marks
+def remove_citation_markers(text):
+    return re.sub(r'\[\d+\]', '', text)
