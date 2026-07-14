520e21b1c522589f545c851b0bb1af521a5c423f
Anshul Garg <anshulgarg1133@gmail.com>
2026-07-14 22:26:40 +0530

    Update utils.py

--- testing_features/utils.py
+++ testing_features/utils.py
@@ -6,11 +6,11 @@ def clean_chunk(text):
     text = re.sub(r'\bPage \d+\b', '', text)    # remove things like "Page 3"
     return text.strip()
 
+#remove citation marks
+def remove_citation_markers(text):
+    return re.sub(r'\[\d+\]', '', text)
+    
 #splitting the answers to check
 def split_into_claims(answer_text):
     sentences = re.split(r'(?<=[.!?])\s+', answer_text)   # split on sentence-ending punctuation
     return [s.strip() for s in sentences if s.strip()]
-
-#remove citation marks
-def remove_citation_markers(text):
-    return re.sub(r'\[\d+\]', '', text)
